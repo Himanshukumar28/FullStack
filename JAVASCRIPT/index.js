f5a7ae7c8c3d8bf5d829b65822d94be617988bee
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-05-19 23:42:54 +0530

    new commit

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -1,1 +1,25 @@
-console.log('hello World')
+// console.log('Hello')
+
+const a  = -1;
+if(a >= 95 && a<= 100){
+    console.log('O')
+}
+else if(a >=90 && a<= 95){
+    console.log('A+')
+}
+else if(a >= 85 && a<= 90){
+    console.log('A')
+}
+else if(a >= 80 && a<= 85){
+    console.log('B+')
+}
+else if(a >= 50 && a<= 79){
+    console.log('B')
+}
+else if(a>100 && a<1){
+    console.log("Invalid")
+}
+else {
+    console.log('FAIL')
+}
+
